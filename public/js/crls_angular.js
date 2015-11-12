ba1ddb02083ec493bf28d2442f069659f6c92e21
simon-baehler <simon.baehler@heig-vd.ch>
2015-11-12 16:16:37 +0100

    ajout d un second graph

--- public/js/crls_angular.js
+++ public/js/crls_angular.js
@@ -11,16 +11,27 @@
 		});
 	});
 })();*/
-angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
+angular.module("app", ["chart.js"])
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-});
+  .controller("LineCtrl", function ($scope) {
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+  })
+
+  .controller("BarCtrl", function ($scope) {
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+  });
 
